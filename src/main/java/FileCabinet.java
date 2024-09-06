3a473aa54512ca1fedc56a40d2aa1f3b4f0c822d
Monika Pustuła <moonika.pustua@gmail.com>
2024-09-06 09:52:38 +0200

    implement methods

--- src/main/java/FileCabinet.java
+++ src/main/java/FileCabinet.java
@@ -4,30 +4,44 @@ import java.util.Optional;
 
 public class FileCabinet implements Cabinet {
     private List<Folder> folders;
+    private final List<Folder> allFolders = getAllFolders();
 
     @Override
     public Optional<Folder> findFolderByName(String name) {
+        for (Folder folder : folders) {
+            if (folder.getName().equals(name)) {
+                return Optional.of(folder);
+            }
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Folder> findFoldersBySize(String size) {
-        return List.of();
+        List<Folder> foldersBySize = new ArrayList<>();
+        for (Folder folder : allFolders) {
+            if (folder.getSize().equals(size)) {
+                foldersBySize.add(folder);
+            }
+        }
+        return foldersBySize;
     }
 
     @Override
     public int count() {
-        return getAllFolders().size();
+        return allFolders.size();
     }
 
     private List<Folder> getAllFolders() {
         List<Folder> allFolders = new ArrayList<>();
-        for (Folder folder : folders) {
-            if (folder instanceof MultiFolder) {
-                List<Folder> subFolders = ((MultiFolder) folder).getFolders();
-                allFolders.addAll(subFolders);
-            } else {
-                allFolders.add(folder);
+        if (folders != null) {
+            for (Folder folder : folders) {
+                if (folder instanceof MultiFolder) {
+                    List<Folder> subFolders = ((MultiFolder) folder).getFolders();
+                    allFolders.addAll(subFolders);
+                } else {
+                    allFolders.add(folder);
+                }
             }
         }
         return allFolders;
